3fe5a227a172996613dd30fd5b77324f68ea48fc
Gabriel S. Jesus <gabriel_live12@outlook.com>
2024-03-11 21:31:37 -0300

    Alterações na função servicos

--- frontend/src/assets/data/servicos.js
+++ frontend/src/assets/data/servicos.js
@@ -1,24 +1,24 @@
 export const servicos = [
     {
-      name: "Cancer Care",
+      name: "Cuidados para crianças",
       desc: "World-class care for everyone. Our health System offers unmatched, expert health care. From the lab to the clinic.",
       bgColor: "rgba(254, 182, 13, .2)",
       textColor: "#FEB60D",
     },
     {
-      name: "Labor & Delivery",
+      name: "Cuidados para Idosos",
       desc: "World-class care for everyone. Our health System offers unmatched, expert health care. From the lab to the clinic.",
       bgColor: "rgba(151, 113, 255, .2)",
       textColor: "#9771FF",
     },
     {
-      name: "Heart & Vascular",
+      name: "Cuidados para PcD",
       desc: "World-class care for everyone. Our health System offers unmatched, expert health care. From the lab to the clinic.",
       bgColor: "rgba(1, 181, 197, .2)",
       textColor: "#01B5C5",
     },
     {
-      name: "Mental Health",
+      name: "Cuidados para Pets",
       desc: "World-class care for everyone. Our health System offers unmatched, expert health care. From the lab to the clinic.",
       bgColor: "rgba(1, 181, 197, .2)",
       textColor: "#01B5C5",
