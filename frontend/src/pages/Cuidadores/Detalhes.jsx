02973e95149811a782cd3d4a812ee1847f4ab906
Gabriel S. Jesus <gabriel_live12@outlook.com>
2024-03-12 18:10:30 -0300

    Atualizações de rotas

--- frontend/src/pages/Cuidadores/Detalhes.jsx
+++ frontend/src/pages/Cuidadores/Detalhes.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 
+import img01 from '../../assets/images/doctor-img02.png';
+
 const Detalhes = () => {
-  return (
-    <div>Detalhes</div>
-  )
+  return <section>
+    <div className="max-w-[1170px] px-5 mx-auto">
+        <div className="grid md:grid-cols-3 gap-[50px]">
+
+          <div className="md:col-span-2"></div>
+            <div className="flex items-center gap-5">
+              <Figure className="max-w-[200px] max-h-[200px]">
+                <img src={img01} alt="Cuidador imagem" />
+              </Figure>
+            </div>
+          <div></div>
+
+        </div>
+    </div>
+  </section>
 }
 
 export default Detalhes;
