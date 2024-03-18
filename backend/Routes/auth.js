5fb68631bb2eed4dbe3e7dc0c6652c955c6ed577
Gabriel S. Jesus <gabriel_live12@outlook.com>
2024-03-18 16:39:56 -0300

    Criação da checagem dos dados do usuario

--- backend/Routes/auth.js
+++ backend/Routes/auth.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { register, login } from '../Controllers/authControllers.js';
+import { register, login } from '../Controllers/authController.js';
 
 const router = express.Router();
 
